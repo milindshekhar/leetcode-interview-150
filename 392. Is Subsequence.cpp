dfface278256854c3acfada482e2e09aebe634e4
Milind Shekhar <43276934+milindshekhar@users.noreply.github.com>
2025-02-05 23:50:56 +0530

    Create 392. Is Subsequence.cpp

---
+++ 392. Is Subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0,j=0;
+        while(j < t.size())
+        {
+            if(i < s.size() && s[i] == t[j])
+            {
+                i++;
+            }
+            j++;
+        }
+        return i == s.size();
+    }
+};
+
+//TC - O(n) t the length of string
+//SC - O(1)
