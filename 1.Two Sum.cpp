544aece795b6bd83c1f8ed503a123a85727cd8dd
Milind Shekhar <43276934+milindshekhar@users.noreply.github.com>
2025-02-15 19:01:10 +0530

    Update 1.Two Sum.cpp

--- 1.Two Sum.cpp
+++ 1.Two Sum.cpp
@@ -20,3 +20,25 @@ public:
 
 //TC - O(n^2)
 //SC - O(1)
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mpp;
+        for(int i=0;i<nums.size();i++)
+        {
+            int moreNeeded = target-nums[i];
+            if(mpp.find(moreNeeded) != mpp.end())
+            {
+                return {mpp[moreNeeded],i};
+            }
+            mpp[nums[i]] = i;
+        }
+    return {-1,-1};
+        
+    }
+};
+
+//TC - O(n) Only 1 iteration.O(1) from finding from hashmap.Worst case scenario Rare takes O(n^2). So to fix that we can use map instead of unordered map.
+//SC - O(n)
