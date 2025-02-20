268a3d23c43b3285e776947aee44da9235dc004d
Milind Shekhar <43276934+milindshekhar@users.noreply.github.com>
2025-02-20 20:48:30 +0530

    Create 172. Factorial Trailing Zeroes.cpp

---
+++ 172. Factorial Trailing Zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int count = 0;
+    while (n >= 5) {
+        count += n / 5;
+        n /= 5;
+    }
+    return count;
+}
+    
+};
+
+//TC - O(n)
+//SC - O(1)
