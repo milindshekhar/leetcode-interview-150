0f0fe0effa21681cc84ae620af7b64728259a2ed
Milind Shekhar <43276934+milindshekhar@users.noreply.github.com>
2025-03-29 19:30:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (58.65%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxValue=INT_MIN;
+        for(int i=0;i<accounts.size();i++)
+        {
+            int wealth=0;
+            for(int j=0;j<accounts[i].size();j++)
+            {
+                wealth+=accounts[i][j];
+            }
+            maxValue=max(wealth,maxValue);
+        }
+        return maxValue;
+    }
+};
