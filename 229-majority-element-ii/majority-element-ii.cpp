749b7e2793b851adeb307ee2e1acfc6557988f17
Milind Shekhar <43276934+milindshekhar@users.noreply.github.com>
2025-04-17 18:58:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (84.82%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,82 +1,57 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-           // Size of the array
-        int n = nums.size(); 
-
-        // Counts for elements el1 and el2
-        int cnt1 = 0, cnt2 = 0;
-        
-        /*Initialize Element 1 and 
-        Element 2 with INT_MIN value*/
-        int el1 = INT_MIN, el2 = INT_MIN;
-
-        /*Find the potential candidates using
-        Boyer Moore's Voting Algorithm*/
-        for (int i = 0; i < n; i++) {
-            
-            if (cnt1 == 0 && el2 != nums[i]) {
-                cnt1 = 1;
-                // Initialize el1 as nums[i]
-                el1 = nums[i]; 
+        int n=nums.size();
+        int count1=0,count2=0,ele1=INT_MIN,ele2=INT_MIN;
+        vector<int> ans;
+        for(int i=0;i<n;i++)
+        {
+            if(count1==0 && ele2!=nums[i])
+            {
+                count1=1;
+                ele1=nums[i];
             }
-            else if (cnt2 == 0 && el1 != nums[i]) {
-                cnt2 = 1;
-                // Initialize el2 as nums[i]
-                el2 = nums[i]; 
-            } 
-            else if (nums[i] == el1) {
-                // Increment count for el1
-                cnt1++;
-            } 
-            else if (nums[i] == el2) {
-                // Increment count for el2
-                cnt2++; 
-            } 
-            else {
-                // Decrement count for el1
-                cnt1--; 
-                 // Decrement count for el2
-                cnt2--;
+            else if(count2==0 && ele1!=nums[i])
+            {
+                count2=1;
+                ele2=nums[i];
+            }
+            else if(ele1 == nums[i])
+            {
+                count1++;
+            }
+            else if(ele2==nums[i])
+            {
+                count2++;
+            }
+            else
+            {
+                count1--;
+                count2--;
             }
         }
-
-        //Validate the candidates by counting occurrences in nums
-        //Reset counts for el1 and el2
-        cnt1 = 0, cnt2 = 0; 
-        
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == el1) {
-                // Count occurrences of el1
-                cnt1++; 
+        count1=0;
+        count2=0;
+        for(int i=0;i<n;i++)
+        {
+            if(ele1==nums[i])
+            {
+                count1++;
             }
-            if (nums[i] == el2) {
-                 // Count occurrences of el2
-                cnt2++;
+            if(ele2==nums[i])
+            {
+                count2++;
             }
         }
-
-        /* Determine the minimum count
-        required for a majority element*/
-        int mini = n / 3 + 1;
-        
-        // List of answers
-        vector<int> result; 
-
-        /*Add elements to the result vector
-        if they appear more than n/3 times*/
-        if (cnt1 >= mini) {
-            result.push_back(el1);
-        }
-        if (cnt2 >= mini && el1 != el2) {
-            // Avoid adding duplicate if el1 == el2
-            result.push_back(el2); 
-        }
-
-        // Uncomment the following line if you want to sort the answer array
-        // sort(result.begin(), result.end()); // TC --> O(2*log2) ~ O(1);
-
-        //return the majority elements
-        return result;
+      int mini=n/3+1;
+      if(count1>=mini)
+      {
+        ans.push_back(ele1);
+      }
+      if(count2>=mini && ele1!=ele2)
+      {
+        ans.push_back(ele2);
+      }
+      return ans;
     }
 };
